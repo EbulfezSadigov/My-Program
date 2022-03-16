a3490ebeae7ad62454212ebbf8922eab7801a2e3
EbulfezSadigov <7ag0he8@code.edu.az>
2022-03-16 23:01:36 +0400

    Method Overloading

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateArea(1,2,3));
+        }
+        static int CalculateArea(int r)
+        {
+            int sum=1, p = 3;
+            sum = p * r;
+            return sum;
+        }
+        static int CalculateArea(int a, int b)
+        {
+            int sum = 1;
+            sum = sum * (a * b);
+            return sum;
+        }
+        static int CalculateArea(int a, int b, int c)
+        {
+            int sum = 1;
+            sum = sum * 2 * (a * b + b * c + a * c);
+            return sum;
+        }
+    }
+}
